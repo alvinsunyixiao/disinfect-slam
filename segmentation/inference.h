101e437db74ff08990ce8e578b62a337428d39d9
RogerQi <17267101+RogerQi@users.noreply.github.com>
2021-01-14 12:22:43 -0600

    remove dead auto merging diffs

--- segmentation/inference.h
+++ segmentation/inference.h
@@ -15,11 +15,7 @@ class inference_engine {
 
         // ret[0]: ht map
         // ret[1]: lt map
-<<<<<<< HEAD
         std::vector<cv::Mat> infer_one(const cv::Mat & rgb_img, bool ret_uint8_flag);
-=======
-        std::vector<cv::Mat> infer_one(const cv::Mat & rgb_img);
->>>>>>> origin/master
     
     private:
         torch::jit::script::Module engine;
