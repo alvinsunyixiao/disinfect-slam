2db45c8daf911e20a4f174f1d3fff3420ee0d5aa
RogerQi <17267101+RogerQi@users.noreply.github.com>
2021-01-19 12:47:52 -0600

    remove some dead code/comments according to reviews in #22

--- examples/tsdf/online.cc
+++ examples/tsdf/online.cc
@@ -179,7 +179,6 @@ void reconstruct(const ZEDNative &zed_native, const L515 &l515,
   std::thread t_slam([&]() {
     cv::Mat img_left, img_right;
     while (true) {
-      //cv::Mat img_left, img_right, img_rgb, img_depth;
       if (SLAM->terminate_is_requested())
         break;
       // get sensor readings
@@ -210,6 +209,8 @@ void reconstruct(const ZEDNative &zed_native, const L515 &l515,
       img_depth.convertTo(img_depth, CV_32FC1, 1. / l515.get_depth_scale());
       std::vector<cv::Mat> prob_map = segmentation_engine->infer_one(img_rgb, false);
       TSDF->Integrate(posecam_P_world, img_rgb, img_depth, prob_map[0], prob_map[1]);
+      spdlog::debug("[Main] Frame integration takes {} ms",
+        get_timestamp<std::chrono::milliseconds>() - timestamp);
     }
   });
 
